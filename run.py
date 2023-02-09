2c30669edff5aa327b2871dace643dbf8cf9d72a
Alain2020 <favouralain@yahoo.com>
2023-02-09 02:31:25 +0000

    Add and oust question to the quiz game

--- run.py
+++ run.py
@@ -1,8 +1,26 @@
 #Solino General knowledge Quiz Game
 
-questions = ()
+questions = ("What’s the fastest land animal in the world?: ",
+             "How many hearts does an octopus have?: ",
+            "What country won the very first FIFA World Cup in 1930?: ",
+            "What color is the tongue of a giraffe?: ",
+            "For which team did Michael Jordan spend most of his career playing?: ",
+            "Which country has the most Olympic gold medals in swimming?: ",
+            "What year did boxing become a legal sport?: ",
+            "Who was Prime Minister of Great Britain from 1841 to 1846?: ",
+            "Name the East African country which lies on the Equator?: ",
+            "Which country is the island of Zanzibar part of?: ")
 
-options = ((),(),(),())
+options = (("A.","B.","C.","D. "),
+           ("A.","B.","C.","D. "),
+           ("A.","B.","C.","D. "),
+           ("A.","B.","C.","D. "),
+           ("A.","B.","C.","D. "),
+           ("A.","B.","C.","D. "),
+           ("A.","B.","C.","D. "),
+           ("A.","B.","C.","D. "),
+           ("A.","B.","C.","D. "),
+           ("A.","B.","C.","D. "))
 
 answers = ()
 guesses = []
