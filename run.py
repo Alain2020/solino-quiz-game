c7cb787f15d1b0e4d85791c49984bae6f8968c74
Alain2020 <favouralain@yahoo.com>
2023-02-09 02:11:09 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Solino General knowledge Quiz Game
+
+questions = ()
+
+options = ((),(),(),())
+
+answers = ()
+guesses = []
+score = 0
+
+question_num = 0 
