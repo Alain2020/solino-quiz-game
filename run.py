8a8e62dda89904c55bdb6b805257f87673c7fbc1
Alain2020 <favouralain@yahoo.com>
2023-02-10 02:10:43 +0000

    Add more questions and made some corrections in the question section

--- run.py
+++ run.py
@@ -34,32 +34,29 @@ for question in questions:
     for option in options[question_num]:
         print(option)
 
-
-   guess = input("Enter (A, B, C, D): ").upper()
-   guesses.append(guess)
-if  guess == answers[question_num]:
-    score += 1
-    print("CORRECT!")
-else:
-    print("INCORRECT!")
-    print(f"{answers[question_num]} is the correct answer")
-question_num += 1
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("CORRECT!")
+    else:
+        print("INCORRECT!")
+        print(f"{answers[question_num]} is the correct answer")
+    question_num += 1
 
 print("----------------------")
-print("        RESULTS       ")
+print("       RESULTS        ")
 print("----------------------")
 
 print("answers: ", end="")
 for answer in answers:
     print(answer, end=" ")
-    print()
+print()
 
-    print("guesses: ", end="")
+print("guesses: ", end="")
 for guess in guesses:
     print(guess, end=" ")
-    print()
+print()
 
-    score  = int(score / len (questions) * 100)
-    print(f"Your score is: {score}%")
-
-
+score = int(score / len(questions) * 100)
+print(f"Your score is: {score}%")
